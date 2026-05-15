38600b629dc2914e4d742c236bdd2d8ee4ab0592
juancruzr-lgtm <juancruzr@mercosurseguridad.com.ar>
2026-05-15 13:13:14 -0300

    Add user prop to SupervisorMobile component

--- components/supervisor/SupervisorMobile.tsx
+++ components/supervisor/SupervisorMobile.tsx
@@ -1,7 +1,10 @@
-export default function SupervisorMobile() {
+'use client'
+
+export default function SupervisorMobile({ user }: any) {
   return (
-    <div style={{ padding: 20 }}>
+    <div style={{ padding: 20, color: '#e2e8f0' }}>
       <h1>Supervisor Mobile</h1>
+      <p>{user?.nombre} {user?.apellido}</p>
     </div>
   )
 }
